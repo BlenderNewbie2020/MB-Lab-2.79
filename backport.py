0e7126cdd046c800b9e5756ab61b4866723b6ac8
Mono Tawake <50071334+tawakemono@users.noreply.github.com>
2019-11-15 12:44:54 +0000

    Update backport.py
    Preparation to future-proof for Blender 2.7 and 2.8 unified coding style

--- backport.py
+++ backport.py
@@ -1,5 +1,6 @@
 import bpy
 
+
 def make_annotations(cls):
     """Converts class fields to annotations if running with Blender 2.8"""
     if bpy.app.version < (2, 80):
@@ -13,6 +14,7 @@ def make_annotations(cls):
             annotations[k] = v
             delattr(cls, k)
     return cls
+
 
 def get_user_preferences(context=None):
     """Multi version compatibility for getting addon keys"""
@@ -28,12 +30,14 @@ def get_user_preferences(context=None):
     else:
         raise Exception("Could not fetch user preferences")
 
+
 def hide_viewport(object, state):
     """Multi version compatibility for setting the viewport hide state of an object"""
     if hasattr(object, "hide_viewport"):
         object.hide_viewport = state # where state is a boolean True or False
     else:
         object.hide = state
+
 
 def set_active_object(context, obj):
     """Get the active object in a 2.7 and 2.8 compatible way"""
@@ -43,12 +47,14 @@ def set_active_object(context, obj):
         context.scene.objects.active = obj # the 2.7 way
     # note that `context.object` still works in 2.8 as a read-only way to get active objects
 
+
 def select_get(object):
     """Multi version compatibility for getting object selection"""
     if hasattr(object, "select_get"):
     	return object.select_get()
     else:
         return object.select
+
 
 def select_set(object, state):
     """Multi version compatibility for setting object selection"""
